e868b5621957ef970f8c23e3a5569c031d589516
Mitch Downey <mitch@everyvote.org>
2013-07-22 00:01:51 -0500

    models.py submit full o' errors
    An attempt at creating the models.py. Some or all of it is wrong,
    especially the ForeignKey and ManyToManyField choices. Also just for the
    exercise I included stuff we don't need in the MVP, like the
    Administrator class.
    
    If you ever get a chance, it'd be great if you could correct this with
    me over VoIP (maybe for like 15-30 mins). In the meantime, I'm going to
    start a Django tutorial, so no rush. Maybe the tutorial will help me
    figure out what I did right and wrong in this models.py.

--- evmini/models.py
+++ evmini/models.py
@@ -2,30 +2,109 @@ from django.db import models
 
 # Create your models here.
 
+
+class ParentConstituency(models.Model):
+    state_name = models.CharField(max_length=30)
+    city_name = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return self.state_name + " " + city_name
+
+        
 class Constituency(models.Model):
-    
-    
-    name = models.TextField()
-    
-    
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+    parentconstituency = models.ForeignKey(ParentConstituency)
     
     def __unicode__(self):
-        
         return self.name
+
     
-
-
-class User(models.Model):
-    name = models.TextField()
-
 class Election(models.Model):
-    
     constituency = models.ForeignKey(Constituency)
-    name = models.TextField()
-    end_date = models.DateTimeField()
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    first_voting_day = models.DateTimeField()
+    last_voting_day = models.DateTimeField()
+        
+    def __unicode__(self):
+        return self.name
+
+        
+class Office(models.Model):
+    election = models.ForeignKey(Election)
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    term_start = models.DateTimeField()
+    term_end = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.name
+        
+        
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    description = models.TextField()
+    # (not sure how to do this...) image = models.ImageField()
+    private_email = models.EmailField()
+    # public_email = models.EmailField()
+    # twitter_name = models.CharField()
+    # facebook_page = models.CharField()
+    # linkedin_page = models.CharField()
+    # personal_homepage = models.CharField()
     
-    moderators = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name        
+    
+    
+class Candidate(models.Model):
+    user = models.ForeignKey(User)
+    # elections = models.ManyToManyField(Electin) (do we need this? or would the offices.id suffice?)
+    offices = models.ManyToManyField(Office)
     
     def __unicode__(self):
+        return self.user
+
         
-        return self.name
+class Moderator(models.Model):
+    user = models.ForeignKey(User)
+    elections = models.ManyToManyField(Election)
+    # do we need offices here? or can those be accessed through elections variable above?
+    
+    def __unicode__(self):
+        return self.user
+        
+
+class Administrator(models.Model):
+    user = models.ForeignKey(User)
+    constituencies = models.ManyToManyField(Constituency)
+    # do we need elections and offices here?
+    
+    def __unicode__(self):
+        return self.user
+        
+        
+class Vote(models.Model):
+    # I really don't understand how the Votes and Comments classes should be setup here...
+    user = models.ForeignKey(User)
+    candidate = models.ForeignKey(Candidate)
+    moderator = models.ForeignKey(Moderator)
+    administator = models.ForeignKey(Administrator)
+    
+    def __unicode__(self):
+        return self.user
+
+        
+class Comment(models.Model):
+    # Uh...hmmm...how?...have no idea when ForeignKey or ManyToManyFields...
+    body = models.TextField()
+    created = models.DateTimeField()
+    user = models.ForeignKey(User)
+    candidate = models.ForeignKey(Candidate)
+    moderator = models.ForeignKey(Moderator)
+    adminstrator = models.ForeignKey(Administrator)
+    
+    def __unicode__(self):
+        return self.body
--- evmini_kai/settings.py
+++ evmini_kai/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/vincent/evmini_kai/evmini.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': 'c:/xampp/htdocs/evmini_kai/evmini.sqlite',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
