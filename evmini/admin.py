d2790bed61be9ed6b10adea272f946c1067e0200
Mitch Downey <mitch@everyvote.org>
2013-07-22 21:57:35 -0500

    I think this is more correct...
    I used a bunch of ManyToManyFields in last commit, and I'm pretty sure
    that's wrong.

--- evmini/admin.py
+++ evmini/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from evmini.models import Election, Constituency
+from evmini.models import Constituency, Election, Office, User, Candidate, Comment
 
-for model in [Election, Constituency]:
-	
+for model in [Constituency, Election, Office, User, Candidate, Comment]:
 	admin.site.register(model)
--- evmini/models.py
+++ evmini/models.py
@@ -3,18 +3,19 @@ from django.db import models
 # Create your models here.
 
 
-class ParentConstituency(models.Model):
-    state_name = models.CharField(max_length=30)
-    city_name = models.CharField(max_length=30)
+# class ParentConstituency(models.Model):
+    # state_name = models.CharField(max_length=30)
+    # city_name = models.CharField(max_length=30)
     
-    def __unicode__(self):
-        return self.state_name + " " + city_name
+    # def __unicode__(self):
+        # return self.state_name + ", " + city_name
 
         
 class Constituency(models.Model):
     name = models.CharField(max_length=75)
     description = models.TextField()
-    parentconstituency = models.ForeignKey(ParentConstituency)
+    # image = ???
+    # parentconstituency = models.ForeignKey(ParentConstituency)
     
     def __unicode__(self):
         return self.name
@@ -24,8 +25,10 @@ class Election(models.Model):
     constituency = models.ForeignKey(Constituency)
     name = models.CharField(max_length=50)
     description = models.TextField()
-    first_voting_day = models.DateTimeField()
-    last_voting_day = models.DateTimeField()
+    first_voting_day = models.DateField()
+    last_voting_day = models.DateField()
+    # is this right? moderators = models.ManyToManyField()
+    # is this right? blockusers = models.ManyToManyField()
         
     def __unicode__(self):
         return self.name
@@ -35,8 +38,8 @@ class Office(models.Model):
     election = models.ForeignKey(Election)
     name = models.CharField(max_length=30)
     description = models.TextField()
-    term_start = models.DateTimeField()
-    term_end = models.DateTimeField()
+    # term_start = models.DateTimeField()
+    # term_end = models.DateTimeField()
 
     def __unicode__(self):
         return self.name
@@ -45,66 +48,75 @@ class Office(models.Model):
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    description = models.TextField()
-    # (not sure how to do this...) image = models.ImageField()
+    # !!! (not sure how to do this...) image = models.ImageField()
     private_email = models.EmailField()
+    # about_me = models.TextField()
     # public_email = models.EmailField()
     # twitter_name = models.CharField()
     # facebook_page = models.CharField()
     # linkedin_page = models.CharField()
     # personal_homepage = models.CharField()
     
-
     def __unicode__(self):
         return self.first_name + " " + self.last_name        
     
     
 class Candidate(models.Model):
     user = models.ForeignKey(User)
-    # elections = models.ManyToManyField(Electin) (do we need this? or would the offices.id suffice?)
-    offices = models.ManyToManyField(Office)
+    election = models.ForeignKey(Election)
+    office = models.ForeignKey(Office)
+    about_me = models.TextField()
     
     def __unicode__(self):
-        return self.user
+        return unicode(self.user)
 
         
-class Moderator(models.Model):
-    user = models.ForeignKey(User)
-    elections = models.ManyToManyField(Election)
-    # do we need offices here? or can those be accessed through elections variable above?
+# class Moderator(models.Model):
+    # user = models.ForeignKey(User)
+    # election = models.ForeignKey(Election)
+    # about_me = models.TextField()
     
-    def __unicode__(self):
-        return self.user
+    # def __unicode__(self):
+        # return unicode(self.user)
         
 
-class Administrator(models.Model):
-    user = models.ForeignKey(User)
-    constituencies = models.ManyToManyField(Constituency)
-    # do we need elections and offices here?
+# class Administrator(models.Model):
+    # user = models.ForeignKey(User)
+    # constituency = models.ForeignKey(Constituency)
+    # about_me = models.TextField()
     
-    def __unicode__(self):
-        return self.user
+    # def __unicode__(self):
+        # return unicode(self.user)
         
-        
-class Vote(models.Model):
-    # I really don't understand how the Votes and Comments classes should be setup here...
-    user = models.ForeignKey(User)
-    candidate = models.ForeignKey(Candidate)
-    moderator = models.ForeignKey(Moderator)
-    administator = models.ForeignKey(Administrator)
-    
-    def __unicode__(self):
-        return self.user
-
         
 class Comment(models.Model):
-    # Uh...hmmm...how?...have no idea when ForeignKey or ManyToManyFields...
+    user = models.ForeignKey(User)
     body = models.TextField()
     created = models.DateTimeField()
-    user = models.ForeignKey(User)
     candidate = models.ForeignKey(Candidate)
-    moderator = models.ForeignKey(Moderator)
-    adminstrator = models.ForeignKey(Administrator)
-    
+    # how do we set up Comment table so user can comment on
+    # moderators, administrators, and other users profiles?
+    # moderator = models.ForeignKey(Moderator)
+    # adminstrator = models.ForeignKey(Administrator)
+    # user2 = how do we comment on another user's profile?
+
     def __unicode__(self):
         return self.body
+        
+
+# class Vote(models.Model):
+    # user = models.ForeignKey(User)
+    # candidate = models.ForeignKey(Candidate)
+    # stance = models.ForeignKey(Stance)
+    # created = models.DateTimeField()
+    
+    # def __unicode__(self):
+        # return self.user
+
+
+# class Stance(models.Model):
+    # name = models.TextField()
+    # description = models.TextField()
+    
+    # def __unicode__(self):
+        # return self.user
